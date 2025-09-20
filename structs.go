4fdf598a09c57846c2356cec97574f5af0236702
crlxs <140880473+crlxs@users.noreply.github.com>
2025-09-20 10:29:01 +0200

    Struct updates

--- structs.go
+++ structs.go
@@ -1,30 +1,44 @@
-// import "time"
-
-import "civil"  //https://pkg.go.dev/cloud.google.com/civil#Date
+import "time"  https://pkg.go.dev/time#Date
+// import "civil"  //https://pkg.go.dev/cloud.google.com/civil#Date
 
 // Exercise
 type Exercise struct {
-	name string
-	targetmuscles []string // A slice of strings
+	Name string
+	Targetmuscles []string // A slice of strings
+	ID int // DB PrimaryKey
 }
 
 // Set
 type Set struct {
-	exercise string
-	repetitions int
-	comment string
-	rir string //Reps In Reserve, how close you brought the exercise to failure.
+	Exercise int // ForeignKey of the exercise
+	Weight float64
+	WeightUnits string
+	Repetitions int
+	Comment string
+	Rir string //Reps In Reserve, how close you brought the exercise to failure.
+	ID int // DB PrimaryKey
 }
 
 // Workout
 type Workout struct {
-	name string
-	exercises []string // A slice of strings
-	duration int
-	date 
+	Name string
+	Sets []Set // A slice of ints, for the foreignkeys of the exercises.
+	Duration int // In seconds, simply transform them to a more readable format when displyaed on the frontend.
+	Date time.Date
+	ID int // DB PrimaryKey
 }
 
 // Split
 type Split struct {
-	name string
+	Name string
+	Workouts []SplitEntry // To tie workouts to days
+	Comment string
+	ID int // DB PrimaryKey
+}
+
+// SplitEntry
+type SplitEntry struct {
+	WorkoutID int
+	Day int
+	ID int // DB PrimaryKey
 }
